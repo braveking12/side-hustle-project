8840a73de2605caace6b85a807bed09b5c95c820
braveking <114780375+braveking12@users.noreply.github.com>
2023-03-26 12:23:45 +0100

    connect function

---
+++ file1.php
@@ -0,0 +1,16 @@
+<?php
+echo "hello guys i am learning php with sidehustle";
+
+
+?>
+<?php
+$connect = mysqli_connect('localhost', 'root', '', 'test');
+
+
+
+if ($connect){
+    echo "connected";
+} else {
+    echo "not connected";
+}
+?>
